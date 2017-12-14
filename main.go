8b2bdc5e5a2bdc896321b37691654d609ed85cb0
Oles Pisarenko <doctornkz@yandex.ru>
2017-12-15 01:17:34 +0300

    Fix db insert leak. Add progress bar.

--- main.go
+++ main.go
@@ -10,12 +10,28 @@ import (
 
 	"github.com/kljensen/snowball"
 	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/cheggaaa/pb.v1"
 )
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func getRows(db *sql.DB, tableName string) int {
+	query, err := db.Prepare("select count(*) from " + tableName)
+	check(err)
+
+	defer query.Close()
+
+	var output int
+	err = query.QueryRow().Scan(&output)
+	if err != nil {
+
+		return -1
+	}
+	return output
 }
 
 func main() {
@@ -51,38 +67,38 @@ func main() {
 	fmt.Println("Db Out initialized")
 
 	// Read file
-
+	fmt.Println("Convering...")
 	readMessages(dbIn, dbOut)
 
 }
 
 func readMessages(db *sql.DB, dbOut *sql.DB) {
+	messagesRows := getRows(db, "messages")
+	importBar := pb.StartNew(messagesRows)
+	wordsRows := getRows(dbOut, "words")
 	rows, err := db.Query("select text from messages")
-	if err != nil {
-		log.Fatal(err)
-	}
+	currentCount := 0
+	check(err)
 	defer rows.Close()
 	for rows.Next() {
 		var text string
 		err = rows.Scan(&text)
-		if err != nil {
-			log.Fatal(err)
-		}
+		check(err)
 
 		for _, Word := range strings.Split(text, " ") {
 
 			Word = stemming(Word)
-			if checkWord(dbOut, Word) == -1 {
+			if checkWord(dbOut, Word) == -10 {
 				insertWord(dbOut, Word)
 			}
-
 		}
-
+		currentCount++
+		importBar.Increment()
 	}
+	importBar.Finish()
+	fmt.Printf("%d rows are processed from Db In. %d words in Db Out.", messagesRows, wordsRows)
 	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 }
 
 func checkWord(db *sql.DB, Word string) int {
@@ -94,28 +110,20 @@ func checkWord(db *sql.DB, Word string) int {
 	var category int
 	err = query.QueryRow(Word).Scan(&category)
 	if err != nil {
-
-		return -1
+		return -10
 	}
 	return category
 }
 
 func insertWord(db *sql.DB, Word string) {
 	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	query, err := tx.Prepare("insert into words(word, categoryid, userid) values(?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	defer query.Close()
 
 	_, err = query.Exec(Word, -1, 0)
-	if err != nil {
-		log.Println(err)
-		log.Fatal(err)
-	}
+	check(err)
 	tx.Commit()
 }
 
